0e0e449eac6dcf5a14757e811cf1e44b8f716d9f
EShimel <edieshimel@gmail.com>
2015-12-01 09:31:13 -0500

    sends prelimenary models/controllers info to git, references mongoose crud for controller data. es

--- models/file.js
+++ models/file.js
@@ -5,9 +5,6 @@
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/file-upload');
 var Schema = mongoose.Schema;
-var crate = require('mongoose-crate');
-var S3 = require('mongoose-crate-s3');
-var env = process.env.NODE_ENV || 'development';
 
 
 var FileSchema = new Schema({
@@ -17,20 +14,3 @@ var FileSchema = new Schema({
     timestamps: true
 });
 
-FileSchema.plugin(crate, {
-    storage: new S3({
-        key: process.env.KEY,
-        secret: process.env.SECRET,
-        bucket: process.env.BUCKET,
-        acl: 'public-read', // defaults to public-read
-        region: 'eu-west-1', // defaults to us-standard
-        path: function(attachment) { // where the file is stored in the bucket - defaults to this function
-            return '/' + attachment.name;
-        }
-    }),
-    fields: {
-        file: {}
-    }
-});
-
-module.exports = mongoose.model('File', FileSchema);
