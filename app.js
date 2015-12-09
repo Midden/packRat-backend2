4509b9e3816c87bb6ec6fd3eba1722f4ebcb025f
Kaylal12 <kaylalei224@gmail.com>
2015-12-09 18:32:31 -0500

    Fixes cors to allow localhost:5000 access

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ var uploadBaseUrl = function (req) {
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(cors({
-  origin: /github\.io/,
+  origin: ['http://localhost:5000', /github\.io/],
   credentials: true
 }));
 app.use(bodyParser.json());
